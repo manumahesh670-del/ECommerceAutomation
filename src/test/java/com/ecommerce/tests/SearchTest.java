76390ee95b8473b147a47129d008570e2bc3b2aa
manu m m <manumahesh670@gmail.com>
2026-08-29 20:48:33 +0530

    Added Data-Driven Test with DataProvider

--- src/test/java/com/ecommerce/tests/SearchTest.java
+++ src/test/java/com/ecommerce/tests/SearchTest.java
@@ -1,30 +1,39 @@
-package com.ecommerce.tests; // Make sure this matches your test package name!
+package com.ecommerce.tests; // Make sure this matches your package name!
 
-import org.testng.annotations.Test;
-import com.ecommerce.pages.HomePage; // This imports the page we just built
-import com.ecommerce.base.BaseTest;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import com.ecommerce.base.BaseTest;
+import com.ecommerce.pages.HomePage;
 
 public class SearchTest extends BaseTest { 
 
-	@Test
-    public void verifyGoogleSearch() throws InterruptedException {
-        // 1. Tell the browser to go directly to Google
-        driver.get("https://www.google.com");
+    // 1. This is the DataProvider: It holds a list of words to test
+    @DataProvider(name = "searchWords")
+    public Object[][] getData() {
+        return new Object[][] {
+            {"laptop"},
+            {"iphone"},
+            {"running shoes"}
+        };
+    }
+
+    // 2. This is the Test: Notice it says dataProvider = "searchWords" now!
+    @Test(dataProvider = "searchWords")
+    public void verifyGoogleSearch(String searchItem) throws InterruptedException {
         
-        // 2. Connect to the HomePage class
+        // Go to Google
+        driver.get("https://www.google.com");
         HomePage homePage = new HomePage(driver);
         
-        // 3. Type "laptop" into Google and hit Enter!
-        homePage.searchForProduct("laptop");
+        // Type the word from our list and hit Enter
+        homePage.searchForProduct(searchItem);
         
-        // 4. Wait 3 seconds for the results page to load
+        // Wait 3 seconds to watch it happen
         Thread.sleep(3000); 
         
-        // 5. NEW: Grab the title of the webpage
+        // Verify the search actually worked
         String actualTitle = driver.getTitle();
-        
-        // 6. NEW: Assert (Verify) that the title contains the word "laptop"
-        Assert.assertTrue(actualTitle.contains("laptop"), "Error: Search results did not load!");
+        Assert.assertTrue(actualTitle.contains(searchItem), "Error: Results failed for " + searchItem);
     }
 }
